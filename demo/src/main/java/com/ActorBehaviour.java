9ef0f59705c832730037ec5bed8194f845dc8eda
Avaddon <servos51261@mail.ru>
2024-09-16 17:38:05 +0300

    16.09.2024

---
+++ demo/src/main/java/com/ActorBehaviour.java
@@ -0,0 +1,8 @@
+package com;
+
+public interface ActorBehaviour {
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean pickUpOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+}
---
+++ demo/src/main/java/com/QueueBehaviour.java
@@ -0,0 +1,8 @@
+package com;
+
+public interface QueueBehaviour {
+    void takeInQueue(Actor actor);
+    void takeOrders();
+    void giveOrders();
+    void releaseFromQueue();
+}
